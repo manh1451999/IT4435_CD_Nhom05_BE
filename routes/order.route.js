727383337787991ce37b622a4751e9fc12193608
Thang <Mxthang1999@gmail.com>
2021-05-25 07:24:40 +0700

    update booking

---
+++ routes/order.route.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var orderModel = require('../models/order.model')
+var orderController = require('../controller/order.controller')
+var authMiddleware = require('../middleware/auth.middleware')
+
+
+router.get('/', authMiddleware.isAuth, orderController.getListOrder)
+router.get('/user', authMiddleware.isAuth, orderController.getOrderByUser)
+router.put('/upadteStatus', authMiddleware.isAuth, orderController.upadteStatus)
+router.delete('/:id', orderController.deleteOrderById)
+
+module.exports = router;
